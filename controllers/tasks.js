e86a441c0ef5a316080efe33c82b2920d5b19cfb
Harris <aungthiha12345mdy@gmail.com>
2022-03-21 11:31:10 +0630

    add method comments

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -6,16 +6,20 @@ const asynWrapper = require('../middlewares/async')
 //Error handler middleware
 const { createCustomError } = require('../errors/custom-error')
 
+//Get method
 const getAllTasks = asynWrapper(async (req, res) => {
     const tasks = await Task.find({})
     res.status(200).json({ tasks })
 })
 
+//Post method
 const createTask = asynWrapper(async (req, res) => {
     const task = await Task.create(req.body)
     res.status(201).json({ task })
 
 })
+
+//Get specific method
 const getSingleTask = asynWrapper(async (req, res, next) => {
     const { id: taskID } = req.params
     const task = await Task.findOne({ _id: taskID })
@@ -26,6 +30,7 @@ const getSingleTask = asynWrapper(async (req, res, next) => {
 
 })
 
+//Delete method
 const deleteTask = asynWrapper(async (req, res, next) => {
     const { id: taskID } = req.params
     const task = await Task.findOneAndDelete({ _id: taskID })
@@ -36,6 +41,7 @@ const deleteTask = asynWrapper(async (req, res, next) => {
 
 })
 
+//Patch method
 const updateTask = asynWrapper(async (req, res, next) => {
     const { id: taskID } = req.params
     const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
